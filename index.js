cd00b4d876d180e82a760773b3d1a39b9d1e0e9d
PabloJoss <josuecallado@hotmail.com>
2023-06-02 23:34:10 -0600

    AVANCE2

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
-const app = express ()
+const app = express ();
+const {employees} = require('./employees.json');
 
 app.get("/",(req,res, next)=>{
-    res.status(200);
-    res.send("Bienvenido");
+   return res.status(200).send("Bienvenido a la base de datos de empleados")
 });
 
-app.listen(3001, ()=>{
+app.get('/employees/all', (req,res,next)=>{
+    return res.status(200).send(employees);
+});
+
+app.get('/employees/:id([0-9]{1,3})', (req,res,next)=>{
+    const id =req.params.id -1;
+    (id >= 0 && id <= 1)?
+        res.status(200).send(employees[req.params.id -1]):
+        res.status(404).send("Empleado no encontrado");
+    
+});
+
+app.get('/employees/:name([A-Za-z]+)', (req,res,next)=>{
+    const name = req.params.name;
+
+    const empl= employees.filter((p)=>{
+        return (p.name.toUpperCase() == name.toUpperCase()) ?p : null;
+    });
+
+    (empl.length>0)? 
+        res.status(200).send(empl):
+        res.status(404).send ("Empledo no encontrado");   
+    
+});
+
+
+app.listen(process.env.PORT || 3001, ()=>{
     console.log("Server is running...");
 });
